efd9bef3647ab40467eb11a72974a4463825e5ea
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-04-01 21:35:13 +0200

    script to convert images started

---
+++ changeImage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#open images in directory and convert them from RGBA to RGB
+
+images_path =  'c:/Users/Miro/My Drive/Python_coursera/7 Final project/catalog_information/supplier-data/images/'
+
+def convert_rgb (images_path):
+
+    
