82f597dd29f294139deabf78c92dcd9e3644d85e
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-04-04 22:23:22 +0200

    uploads files to server

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -3,19 +3,21 @@ import requests
 import os
 import glob
 
-# This example shows how a file can be uploaded using
-# The Python Requests module
-
-# url = "http://localhost/upload/"
-# with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
-#     r = requests.post(url, files={'file': opened})
-
-
 # loop over files in images path and send them to server, open only .jpeg files
 
+url = "http://localhost:8000/"
 images_path =  'c:/Users/Miro/My Drive/Python_coursera/7 Final project/catalog_information/supplier-data/images/*.jpeg'
 
 for images in glob.glob(images_path):
-    print(images)
+
+    input_path = os.path.join(images_path, images)
+
+    print(input_path)
+
+    with open(input_path, "rb") as opened:
+
+        r = requests.post(url, files={'file': opened})
+
+
 
 
