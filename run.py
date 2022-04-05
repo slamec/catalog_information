e1e8076f055291b5312b1f8ba48ab85288aeb0ae
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-04-05 22:06:34 +0200

    first commit of run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python3
+
+import os
+import requests
+import glob                             
+
+text_path = 'c:/Users/Miro/My Drive/Python_coursera/7 Final project/catalog_information/supplier-data/images/*.txt'
+fruit_information = {}
+
