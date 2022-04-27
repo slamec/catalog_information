beb38e544657d9864ca2b81c0c78065c7f108f66
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-04-27 21:42:30 +0200

    returns disk usage

--- health_check.py
+++ health_check.py
@@ -1,13 +1,36 @@
 # !/usr/bin/env python3
 
+from turtle import st
 import psutil
 import shutil
 import email
 
-# check cpu usage
+# check cpu usage if higher than 80% return actual usage
 def cpu_usage():
-    cpu = psutil.cpu_percent(60)
+    cpu = psutil.cpu_percent(5)
 
-    return cpu
-
+    if cpu >= 80:
+        pass
+    else:
+        return "CPU usage is lower than 80%"
+    
 print(cpu_usage())
+
+# checl disk usage
+def disk_space():
+    total, used, free = shutil.disk_usage(__file__)
+
+    usage_perc = float(used)/float(total) * 100
+
+    if usage_perc <= 20:
+
+        free_space = "{:.2f}{}".format(usage_perc, '%')
+        pass
+    else:
+        return "Disk space is less than 20%"
+
+print(disk_space())
+
+
+
+
